4e964310a6cb33a503088585b5e5d44450d22a02
plyte <boywithstickx@yahoo.com>
2016-09-21 00:15:34 -0700

    Update

--- Driver.py
+++ Driver.py
@@ -3,19 +3,30 @@
 # Purpose: To house the Main function and any other initializations made
 # Date: 08/25/16
 
-import sOE
 import Request
-import requests
-import json
-import string
+import ParseData
+import pprint
 
 def main():
 
-    nameimput = 'céll'
-    summoner = Request.Request(name=nameimput)
-    matchlist = Request.Request.callmatchlist(summoner.callid, 2015)
-    print(summoner.callid)
-    print(matchlist)
+    # input a name from the webpage
+    #nameimput = 'céll'
+
+    # assign the
+    #getid = Request
+    #setid = getid.Request(name=nameimput)
+    #summonerid = setid.callid()
+    #print(summonerid)
+    #time.sleep(10)
+    #getmatchlist = Request
+    #matchlist = getmatchlist.Request.callmatchlist(24196132, 2015)
+    #print(matchlist)
+    #time.sleep(10)
+    #time.sleep(10)
+    #pp = pprint.PrettyPrinter(indent=4)
+    ParseData.gettimelinedata
+    #print(matchlist)
+
 
 
 if __name__ == "__main__":
