55e7eabf58300962e92a13d578629b7c2a5f4fb5
plyte <boywithstickx@yahoo.com>
2016-09-21 00:18:58 -0700

    Revert "Revert "Update""
    This reverts commit 68a7f91ea56496e66b6eec14683d09e7b090d0c9.

--- Request.py
+++ Request.py
@@ -6,45 +6,51 @@
 import requests
 import Consts
 import json
+import sOE
 
 
-class Request:
+def callid(name):
+    key = Consts.URL['key']
+    region = Consts.REGION['North_America']
+    version = Consts.VERSION['version']
+    url = Consts.URL['summoner'].format(
+        region=region,
+        key=key,
+        version=version,
+        ame=name
+    )
+    apiobjectci = requests.get(url)
+    jsondata = dict(json.loads(apiobjectci.text))
+    sumid = jsondata[name]['id']
 
-    # initialize the variables
-    def __init__(self, name):
-        self.name=name
+    return sumid
 
-    # utilize the requests module to call the api
 
-    @property
-    def callid(self):
-        key = Consts.URL['key']
-        region = Consts.REGION['North_America']
-        version = Consts.VERSION['version']
-        url = Consts.URL['summoner'].format(
-            region=region,
-            key=key,
-            version=version,
-            name=self.name
-        )
-        apiobjectci = requests.get(url)
-        jsondata = dict(json.loads(apiobjectci.text))
+def callmatchlist(sumid,year):
+    key = Consts.URL['key']
+    region = Consts.REGION['North_America']
+    url = Consts.URL['matchlist'].format(
+        region=region,
+        key=key,
+        sumid=sumid,
+        typeOfRanked=Consts.URL['RANKED_SOLO_5x5'],
+        year=year
+    )
+    apiobjectml = requests.get(url)
+    matchlist = dict(json.loads(apiobjectml.text))
 
-        sumid = jsondata[self.name]['id']
+    return matchlist
 
-        return sumid
 
-    def callmatchlist(sumID,year):
-        key = Consts.URL['key']
-        region = Consts.REGION['North_America']
-        url = Consts.URL['matchlist'].format(
-            region=region,
-            key=key,
-            sumID=sumID,
-            typeOfRanked=Consts.URL['RANKED_SOLO_5x5'],
-            year=year
-        )
-        apiobjectml = requests.get(url)
-        matchlist = dict(json.loads(apiobjectml.text))
+def specificmatchinfo(matchid):
+    key = Consts.URL['key']
+    region = Consts.REGION['North_America']
+    url = Consts.URL['match'].format(
+        region=region,
+        key=key,
+        matchid=matchid,
+    )
+    apiobjectm = requests.get(url)
+    match = dict(json.loads(apiobjectm.text))
 
-        return matchlist
+    return match
